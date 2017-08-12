60faa602149aa366351b7fc72b1a6986ced929f0
Kelemen Márton <kelemenmarton@Kelemen-MacBook-Pro.local>
2017-08-12 11:59:57 +0200

    init comm 2

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const publicPath = path.join(__dirname, '..', '/public');
+const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
 
