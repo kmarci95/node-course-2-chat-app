a894c33475b4e0dfdece39c7cd1bc8c40f7425b8
Kelemen Márton <kelemenmarton@Kelemen-MacBook-Pro.local>
2017-08-13 11:28:37 +0200

    Emit new message on createMessage

--- server/server.js
+++ server/server.js
@@ -21,24 +21,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
     
-    socket.emit('newEmail', {
-        from: 'mike@example.com',
-        text: 'Whats up?',
-        createAt: 123
-    });
+//    socket.emit('newEmail', {
+//        from: 'mike@example.com',
+//        text: 'Whats up?',
+//        createAt: 123
+//    });
     
-    socket.emit('newMessage', {
-        from: 'Lynch',
-        text: 'Damn good cafe',
-        createdAt: 123
-    });
+//    socket.emit('newMessage', {
+//        from: 'Lynch',
+//        text: 'Damn good cafe',
+//        createdAt: 123
+//    });
     
-    socket.on('createEmail', (newEmail) => {
-        console.log('createEmail', newEmail);
-    });
+//    socket.on('createEmail', (newEmail) => {
+//        console.log('createEmail', newEmail);
+//    });
     
-    socket.on('createMessage', (newMessage) => {
-        console.log('createMessage', newMessage);
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
     
     socket.on('disconnect', () => {
